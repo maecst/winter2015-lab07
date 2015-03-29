3a1df4f859331e56e5294a55c4e07214aff68987
Mae Yee <mae.cast@hotmail.com>
2015-03-29 11:53:41 -0700

    * edited views, added a third xml order Signed-off-by:Mae Yee <mae.cast@hotmail.com>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,3 +1,11 @@
 <div class="row">
-    Burger Bonanza?
+    <h3> List of Orders </h3>
+       
+    <ul>
+        {orders}
+        <li><a href="/welcome/order/{filename}"> Order #{filename} for {customer}  </a></li>
+        {/orders}
+    </ul>
+    
+    <p> Select an order from the list above to see its receipt. </p>
 </div>
--- application/views/justone.php
+++ application/views/justone.php
@@ -1,3 +1,50 @@
 <div class="row">
-A burger?
+
+    <h3> Order #{filename} for {customer} ({type}) </h3>
+    
+    <p>Order Notes: <em>{special}</em> </p>
+    
+    {burgers}
+    <br>
+    <h4><strong> * * * * * * * * * &nbsp;&nbsp; Burger # {count} &nbsp;&nbsp; * * * * * * * * * </strong></h4>
+
+    <table>
+        <tr>
+            <td width="4%"> Base: </td>
+            <td width="30%"> {patty} </td>
+        </tr>
+        <tr>
+            <td> Cheeses: </td>
+            <td> {top-cheese} {bottom-cheese} </td>
+        </tr>
+        <tr>
+            <td> Toppings: </td>
+            <td> {toppings} </td>
+        </tr>
+        <tr>
+            <td> Sauces: </td>
+            <td> {sauces}
+        <tr>
+            <td> Instructions: </td>
+            <td> {instructions} </td>
+        </tr>
+        <tr>
+            <td> &nbsp;</td>
+        </tr>
+        <tr>
+            <td> Burger total: </td>
+            <td> $ {total} </td>
+        </tr>
+    </table>
+    {/burgers}
+    
+    <br>
+    <h4><strong> 
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * <br>
+        Order TOTAL:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $ {total} <br>
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * <br>
+    </strong></h4>
+    
+    <p><a href="/welcome"> Back to List of Orders </a></p>
+    
 </div>
